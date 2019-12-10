be573a1e7485ca85ac373c2c13c738831881a1df
Nur331 <57195710+Nur331@users.noreply.github.com>
2019-12-10 19:47:55 +0100

    updated createEvent class

--- src/test/java/tests/createEvent.java
+++ src/test/java/tests/createEvent.java
@@ -1,4 +1,10 @@
 package tests;
 
 public class createEvent {
+  
+  public static void main(String [] args){
+  System.out.println("Hello world ");
+  // thi line is added from github , see you in intellij  ;)
+  
+  }
 }
